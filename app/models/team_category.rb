d45618a93c311e75de2901f534a53a3ebf519253
John H Wallace <john.wallace@hamnavoepartners.com>
2022-10-04 13:59:27 +0100

    add team category lookup

---
+++ app/models/team_category.rb
@@ -0,0 +1,2 @@
+class TeamCategory < ApplicationRecord
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+unless TeamCategory.find_by(name: "International")
+  TeamCategory.create(name: "International")
+end
