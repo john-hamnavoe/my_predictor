98bda400f49994343c448653092f8e0429966890
John H Wallace <john.wallace@hamnavoepartners.com>
2022-10-25 09:59:14 +0100

    make sure devise alert shown, and error messages shown

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -2,4 +2,6 @@ class Fixture < ApplicationRecord
   belongs_to :home_team, class_name: "Team"
   belongs_to :away_team, class_name: "Team"
   belongs_to :competition
+
+  validates :date, presence: true
 end
