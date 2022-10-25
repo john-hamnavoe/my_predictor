547394e76624ccc6496af839882e55ad3b8ce914
John H Wallace <john.wallace@hamnavoepartners.com>
2022-10-25 23:14:55 +0100

    formatting on fixture form

--- app/components/forms/select_input_component.rb
+++ app/components/forms/select_input_component.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class Forms::SelectInputComponent < Forms::BaseComponent
-  private attr_reader :options, :include_blank, :classes, :read_only
+  private attr_reader :options, :include_blank, :classes, :read_only, :rounded
 
-  def initialize(form, field, options:, classes: nil, include_blank: false, read_only: false)
+  def initialize(form, field, options:, classes: nil, include_blank: false, read_only: false, rounded: false)
     super(form, field)
     @options = options
     @include_blank = include_blank
     @classes = classes
     @read_only = read_only
+    @rounded = rounded
   end
 
   def call
@@ -17,7 +18,11 @@ class Forms::SelectInputComponent < Forms::BaseComponent
 
   private 
 
+  def rounded_class 
+    @rounded ? "rounded-md" : "rounded-none"
+  end
+
   def input_class
-    "#{@classes} disabled:bg-gray-200 relative block w-full appearance-none rounded-md border border-gray-300 px-2 py-1 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+    "#{@classes} disabled:bg-gray-200 relative block w-full appearance-none #{rounded_class} border border-gray-300 px-2 py-1 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
   end
 end
