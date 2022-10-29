dd452a996baa07b4ac311877a2730586a8b5cc20
John H Wallace <john.wallace@hamnavoepartners.com>
2022-10-29 10:09:05 +0100

    add the about section

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'upcoming_fixtures/show'
+
   root "home#index"
+  get 'abouts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   devise_for :users
